e5804d8c85d06c7b5e64c473161e170f2c170531
Satish kumar sugasi <ssugas@codeaurora.org>
2016-09-27 17:54:37 -0700

    onclite: bluetooth: Increase MAX ACL connections to 16
    This allows 16 ACL links instead of 7
    
    Change-Id: I7bf04515b5822c51be73ec95cba7975e046bca23

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,7 +20,7 @@
 #define BTM_DEF_LOCAL_NAME   "Redmi 7"
 
 // Disables read remote device feature
-#define MAX_ACL_CONNECTIONS    7
+#define MAX_ACL_CONNECTIONS    16
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
