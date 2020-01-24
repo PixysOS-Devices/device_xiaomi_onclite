d3ec56fdb9655964109dfc19961dea3e1cb27650
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    onclite: Use Pixel charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>

--- device.mk
+++ device.mk
@@ -263,6 +263,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NotchBarKiller
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
