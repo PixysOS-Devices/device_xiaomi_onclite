94708339e06c4aa7acd75ba6bda177ef0f1ddda3
Dhina17 <dhinalogu@gmail.com>
2020-03-01 22:53:15 +0530

    Revert "onclite: Drop Snap"
    This reverts commit 2d2831d0de1297c903e8cd372038fb81c1f7417c.
    * Snap added back to AEX
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libgui_vendor
+    libgui_vendor \
+    Snap
 
 # Configstore
 PRODUCT_PACKAGES += \
