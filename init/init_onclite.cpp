97a6dcf43e90ade660c5c7cb81da1a14a5dc94a2
Dhina17 <dhinalogu@gmail.com>
2020-03-18 17:09:40 +0530

    onclite: Set build fingerprint to bonito from march patch
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- init/init_onclite.cpp
+++ init/init_onclite.cpp
@@ -59,5 +59,5 @@ void vendor_load_properties()
 {
     // fingerprint
     property_override("ro.build.description", "onc-user 9 PKQ1.181021.001 V10.3.4.0.PFLMIXM release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/bonito/bonito:10/QQ2A.200305.002/6138846:user/release-keys");
 }
