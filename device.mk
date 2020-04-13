a5378331c28ff3d208648bb4a1af7c2e434e17f5
Sonal Singh <sonal.singh.19993@gmail.com>
2020-04-13 13:06:48 +0000

    onclite: prebuilts: Add GoogleCameraGo
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I190fa16736e28be15e4100290c15a3004cf7cf94

--- device.mk
+++ device.mk
@@ -180,6 +180,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# Gcam
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
