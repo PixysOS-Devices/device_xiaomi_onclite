5a52a70f239cf54b20bdd25260c83f899ebe86ed
Dhina17 <dhinalogu@gmail.com>
2020-06-06 15:51:01 +0000

    onclite: Split system and vendor props
    * And also cleanup
    
    Change-Id: Ic6c34be3f5ffc068543211b54acdc8b1da290dff
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- device.mk
+++ device.mk
@@ -284,9 +284,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
-# Properties
--include device/xiaomi/onclite/vendor_prop.mk
-
 # QMI
 PRODUCT_PACKAGES += \
     libjson
