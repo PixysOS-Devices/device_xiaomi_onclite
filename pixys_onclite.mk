e7387900e2ca8985d2e59de7e5d9142795d1fa8c
AbhinandAK350 <abhinandak@outlook.com>
2020-11-13 07:55:25 +0100

    PixysOS 4
    Signed-off-by: AbhinandAK350 <abhinandak@outlook.com>

--- pixys_onclite.mk
+++ pixys_onclite.mk
@@ -12,11 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixys stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+
+#Boot animation
+TARGET_BOOT_ANIMATION_RES := 720
+
+#Gapps
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onclite
+PRODUCT_NAME := pixys_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
