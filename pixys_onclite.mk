9448047f93751351399308b953d4710de6577e9e
AbhinandAK350 <abhinandak@outlook.com>
2021-09-19 19:56:56 +0530

    onclite: Update fp to redfin September

--- pixys_onclite.mk
+++ pixys_onclite.mk
@@ -28,9 +28,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.005/7349499:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.005 7349499 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
