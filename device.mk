988f1ad9dcd238e9d143db3c2f7a9176bf3bd11d
xyyx <xyyx@mail.ru>
2021-10-12 09:51:07 +0530

    onclite: Add basic USB HAL that reports no status change
    Signed-off-by: ItsVixano <vixanonew@gmail.com>
    Signed-off-by: soumyajit007-creator <soumyajitpaul001@gmail.com>
    Change-Id: I46874b1d6a6491d07b7d5ae896a192312b4047cd

--- device.mk
+++ device.mk
@@ -397,7 +397,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.msm8953
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
