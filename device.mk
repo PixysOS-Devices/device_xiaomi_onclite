2a3bd0f581159c698b33a17c72a9a2e3fd03bd3b
xawlw <abdulazizawlw@gmail.com>
2021-11-17 09:51:07 +0530

    onclite: vintf: Uprev btconfigstore to 2.0
    Change-Id: Iea0dabd8a84efc253d178eb4b0b6f1c95b927919

--- device.mk
+++ device.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
