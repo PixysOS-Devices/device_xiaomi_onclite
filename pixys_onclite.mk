50ccb67ebca4fa12e492494431af73bbbdb81dd2
Abhinand A K <abhinandak@outlook.com>
2022-03-27 09:23:31 +0530

    onclite: Update build fp to raven April

--- pixys_onclite.mk
+++ pixys_onclite.mk
@@ -28,9 +28,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.003 8210211 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
